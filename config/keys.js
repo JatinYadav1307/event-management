987c678268b322511d35463f582eb9fab3bf23b6
Jatin Yadav <Jatin.Yadav1307@gmail.com>
2017-12-25 21:20:40 +0530

    Configured MongoDB and connection

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     googleClientID: '132995255513-74orejtnmldhnn8pm4nckhmn49tg32k0.apps.googleusercontent.com',
-    googleClientSecret: 'cTUeQJOVaJ6MnBPK_gRq8c3x'
+    googleClientSecret: 'cTUeQJOVaJ6MnBPK_gRq8c3x',
+    mongoURI: 'mongodb://root:root@ds038547.mlab.com:38547/database-dev'
 };
