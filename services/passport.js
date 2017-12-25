00887e6fef1d6d2167105681dcc0f9cd346f9103
Jatin Yadav <Jatin.Yadav1307@gmail.com>
2017-12-25 21:28:02 +0530

    Configured user and saving google ID as primary

--- services/passport.js
+++ services/passport.js
@@ -1,17 +1,18 @@
 const passport = require('passport');
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy(
     {
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-    }, 
+    },
     function (accessToken, refreshToken, profile, done) {
-        console.log('Access Token: ', accessToken)
-        console.log('Refresh Token: ', refreshToken)
-        console.log('User Profile: ', profile)
+        new User({googleId: profile.id}).save()
     }
 )
 );
